eda3ef5715646379c1f764b282f335264babba5e
Gabriel Rea <gabriel.rea96@gmail.com>
2022-05-19 20:29:18 -0300

    first commit

---
+++ Ejercicio-1/src/com/ejercicio1/EjercicioMain.java
@@ -0,0 +1,28 @@
+package com.ejercicio1;
+
+public class EjercicioMain {
+
+//    Para este primer ejercicio tendréis que realizar lo siguiente:
+//    - Crea un proyecto de Java desde 0
+//    - Dentro del proyecto tenéis que crear un paquete. En el paquete tendréis que crear una clase.
+//    - Dentro de la clase tenéis que crear el método main e imprimir todos los datos que se han visto en las sesiones.
+//    - Recordatorio: Los tipos de datos más comunes son int, long, double, boolean, String.
+
+    public static void main(String[] args) {
+
+        String nombre = "Gabriel";
+        String apellido = "Rea";
+        int edad = 26;
+        double peso = 56.25;
+        boolean isDev = true;
+
+        System.out.println("Nombre: " + nombre);
+        System.out.println("apellido: " + apellido);
+        System.out.println("Edad: " + edad);
+        System.out.println("Peso: " + peso);
+        System.out.println("Es desarrollador: " + isDev);
+
+
+    }
+
+}
